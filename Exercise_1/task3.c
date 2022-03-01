1b7fafe6a7135dac3b9a49b3c9fd85bbbaec5580
Miroshima <miro.vannini@protonmail.ch>
2022-03-01 10:09:16 +0100

    ume

--- Exercise_1/task3.c
+++ Exercise_1/task3.c
@@ -36,23 +36,38 @@ void selectionSort(int A[], int n) {
 }
 
 void evenOddSelectionSort(int A[], int n){
-	int E[100];
-	int O[100];
-	int size = n;
 	int counter_e = 0;
 	int counter_o = 0;
 
-	for (int i = 0; i < size; i++){
+	for (int i = 0; i < n; i++){
 		if (A[i] % 2 == 0){
-			E[counter_e] = A[i];
-			counter_e++;
+			counter_e += 1;
 		}else{
-			O[counter_o] = A[i];
-			counter_o++;
+			counter_o += 1;
 		}
 	}
+	int E[counter_e];
+	int O[counter_o];
+	counter_e = 0;
+	counter_o = 0;
+
+	for (int i = 0; i < n; i++){
+		if (A[i] % 2 == 0){
+			E[counter_e] = A[i];
+			counter_e += 1;
+		}else{
+			O[counter_o] = A[i]; 
+			counter_o += 1;
+		}
+	}
+
 	int size_e = sizeof(E) / sizeof(int);
 	int size_o = sizeof(O) / sizeof(int);
+
+	printf("Even Array: \n");
+	printArray(E, size_e);
+	printf("Odd Array: \n");
+	printArray(O, size_o);
 
 	printf("Sorted even numbers: ");
 	selectionSort(E, size_e);

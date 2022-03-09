12892348d057a356ae1e0261d474c0a9120ffd13
Miroshima <miro.vannini@protonmail.ch>
2022-03-09 14:56:09 +0100

    update

--- Exercise_2/task3_skeleton.c
+++ Exercise_2/task3_skeleton.c
@@ -8,15 +8,12 @@
 #include <stdio.h>
 
 int blinks(int n){
-	int res;
-	if (n <= 0){return 0;}
+	
 	if (n == 1){return 1;}
 	if (n == 2){return 2;}
-	if (n == 3){return 3;}
 	else{
-		res = blinks(n-1) + blinks(n-2);
+		return blinks(n-1) + blinks(n-2);
 	}
-	return res;
 }
 
 int main() {

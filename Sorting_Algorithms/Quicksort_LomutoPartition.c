c67e5c40a6b270c6fc54aaba790bccde116d21be
Miroshima <miro.vannini@protonmail.ch>
2022-03-30 13:08:31 +0200

    windows isch indeed scheisse

--- Sorting_Algorithms/Quicksort_LomutoPartition.c
+++ Sorting_Algorithms/Quicksort_LomutoPartition.c
@@ -4,7 +4,7 @@
 void printArray(int arr[], int size){
     int i;
 
-    for (i = 0; i < size; i++){
+    for (i = 0; i <= size; i++){
         printf("%d ", arr[i]);
     }
     printf("\n");
@@ -43,7 +43,7 @@ void QuickSort(int A[], int l, int r){
 
 int main(){
     int A[] = {11, 0, 9 ,19, 8, 1, 5, 13, 18, 7};
-    int size = sizeof(A) / sizeof(A[0]);
+    int size = sizeof(A) / sizeof(A[0]) - 1;
 
     printf("Array before Quicksort: ");
     printArray(A, size);

14c16ca3050b7824c9ed5e9be9c61501cfed56f9
Miroshima <miro.vannini@protonmail.ch>
2022-04-12 14:47:59 +0200

    ex6 + 7 upload

---
+++ Exercise_7/task2_skeleton.c
@@ -0,0 +1,19 @@
+/****************************************************************************
+ * University of Zurich, Informatics II, Spring Semester 2022               *
+ * Exercise 7 - Task 2                                                      *
+ *                                                                          *
+ * @author <your name>                                                      *
+ ****************************************************************************/
+
+typedef struct Stack {
+	unsigned int capacity;
+	int* items;
+  	int top;
+} Stack;
+
+//TODO: your implementation
+
+int main() {
+	//TODO: your implementation
+  return 0;
+}

7179a02710db726884429d00f0b137e5b84c7d45
Miroshima <miro.vannini@protonmail.ch>
2022-04-12 14:12:45 +0200

    min und max heapify

--- Sorting_Algorithms/Heapsort.c
+++ Sorting_Algorithms/Heapsort.c
@@ -18,7 +18,7 @@ int right(int x){
     return x * 2 + 2;
 }
 
-void heapify(int A[], int i, int n){
+void min_heapify(int A[], int i, int n){
     int m, l, r, temp;
 
     m = i;
@@ -35,7 +35,28 @@ void heapify(int A[], int i, int n){
         temp = A[i];
         A[i] = A[m];
         A[m] = temp;
-        heapify(A, m, n);
+        min_heapify(A, m, n);
+    }
+}
+
+void max_heapify(int A[], int i, int n){
+    int m, l, r, temp;
+
+    m = i;
+    l = left(i);
+    r = right(i);
+
+    if (l < n && A[l] < A[m]){
+        m = l;
+    }
+    if (r < n && A[r] < A[m]){
+        m = r;
+    }
+    if (i != m){
+        temp = A[i];
+        A[i] = A[m];
+        A[m] = temp;
+        max_heapify(A, m, n);
     }
 }
 
@@ -43,7 +64,7 @@ void buildHeap(int A[], int n){
     int i;
 
     for (i = n / 2; i >= 0; i--){
-        heapify(A, i, n);
+        max_heapify(A, i, n);
     }
 }
 
@@ -58,7 +79,7 @@ void heapsort(int A[], int n){
         A[i] = A[0];
         A[0] = temp;
         s--;
-        heapify(A, 0, s);
+        max_heapify(A, 0, s);
     }
 }
 

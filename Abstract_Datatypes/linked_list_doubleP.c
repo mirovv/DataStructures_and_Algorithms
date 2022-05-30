1647e0288cc01a66022907566856865ae629e174
Miroshima <miro.vannini@protonmail.ch>
2022-05-30 12:21:37 +0200

    asdf

--- Abstract_Datatypes/linked_list_doubleP.c
+++ Abstract_Datatypes/linked_list_doubleP.c
@@ -11,7 +11,6 @@ void insert(Node** root, int val){
     newNode->val = val;
     newNode->next = NULL;
     if(*root == NULL){
-        printf("hää");
         *root = newNode;
     }else{
         newNode->next = *root;
@@ -32,79 +31,96 @@ void printLL(Node** head){
 }
 
 void merge(Node** l1, Node** l2){
-    Node* curr;
-    Node* next;
-    Node* other;
+    Node* s = NULL;
+    Node* n = NULL;
+    Node* o = NULL;
     if(*l1 && *l2 == NULL){
-        printf("he");
+        printf("bruh");
         return;
     }
-    else if(*l1 == NULL){
-        printf("wieso");
+    else if(*l1 == NULL && *l2 != NULL){
+        printf("asdf");
         *l1 = *l2;
         return;
     }
-    else if(*l2 == NULL){
-        printf("uncool");
+    else if(*l2 == NULL && *l1 != NULL){
+        printf("wieso");
         *l2 = *l1;
         return;
-    }else if((*l1)->val < (*l2)->val){
-        Node* curr = *l1;
-        Node* next = (*l1)->next;
-        Node* other = *l2;
-        printf("were here");
-    }else{
-        Node* curr = *l2;
-        Node* next = (*l2)->next;
-        Node* other = *l1;
     }
-    while(next && other != NULL){
-        if(next->val < other->val){
-            curr = curr->next;
-            next = next->next;
-        }else{
-            curr->next = other;
-            other = other->next;
+    else if((*l1)->val < (*l2)->val){
+        Node* s = *l1;
+        Node* n = (*l1)->next;
+        Node* o = *l2;
+        while(n && o != NULL){
+            if(n->val < o->val){
+                n = n->next;
+                s = s->next;
+            }else{
+                s->next = o;
+                o = o->next;
+            }
+            s = s->next;
         }
-        curr = curr->next;
+        if(n != NULL){
+            s->next = o;
+        }
+        if(o != NULL){
+            s->next = n;
+        }
     }
-    if(next != NULL){
-        other->next = next; 
+    else{
+        Node* s = *l2;
+        Node* n = (*l2)->next;
+        Node* o = *l1;
+        while(n && o != NULL){
+            if(n->val < o->val){
+                n = n->next;
+                s = s->next;
+            }else{
+                s->next = o;
+                o = o->next;
+            }
+            s = s->next;
+        }
+        if(n != NULL){
+            s->next = o;
+        }
+        if(o != NULL){
+            s->next = n;
+        }
     }
-    if(other != NULL){
-        curr->next = other;
-    }
-    printf("ume?");
-    printf("Merged list: \n");
+
 }
 
 
 int main(){
-    Node* l1 = malloc(sizeof(Node));
-    Node* l2 = malloc(sizeof(Node));
+    Node** l1 = malloc(sizeof(Node));
+    Node** l2 = malloc(sizeof(Node));
+    *l1 = NULL;
+    *l2 = NULL;
 
-    insert(&l1, 83);
-    insert(&l1, 77);
-    insert(&l1, 33);
-    insert(&l1, 23);
-    insert(&l1, 8);
-    insert(&l1, 3);
-    insert(&l1, 2);
-    insert(&l1, 1);
+    insert(l1, 83);
+    insert(l1, 77);
+    insert(l1, 33);
+    insert(l1, 23);
+    insert(l1, 8);
+    insert(l1, 3);
+    insert(l1, 2);
+    insert(l1, 1);
 
-    insert(&l2, 69);
-    insert(&l2, 43);
-    insert(&l2, 21);
-    insert(&l2, 19);
-    insert(&l2, 7);
-    insert(&l2, 2);
-    insert(&l2, 1);
-    insert(&l2, 0);
+    insert(l2, 69);
+    insert(l2, 43);
+    insert(l2, 21);
+    insert(l2, 19);
+    insert(l2, 7);
+    insert(l2, 2);
+    insert(l2, 1);
+    insert(l2, 0);
 
     printf("List 1: \n");
-    printLL(&l1);
+    printLL(l1);
     printf("List 2: \n");
-    printLL(&l2);
-    merge(&l1, &l2);
-
+    printLL(l2);
+    merge(l1,l2);
 }
